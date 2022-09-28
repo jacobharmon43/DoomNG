725a7716a70fcc5478b41018b45bcc284abfd6b9
jacobharmon43 <64942817+jacobharmon43@users.noreply.github.com>
2022-09-27 23:40:41 -0500

    Added function to parse for entities with multiple components

--- DoomNG/Engine/Systems/EntityManager.cs
+++ DoomNG/Engine/Systems/EntityManager.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DoomNG.Engine.Systems
 {
-    /// <summary>
-    /// Storage and delivery system for entities in the game
-    /// </summary>
-    /// <seealso cref="DoomNG.Engine.ISystem" />
     internal class EntityManager : ISystem
     {
         readonly List<Entity> _entities;
@@ -14,9 +11,6 @@ namespace DoomNG.Engine.Systems
         Dictionary<Entity, List<IComponent>> _components;
         int _nextId = 0;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EntityManager"/> class.
-        /// </summary>
         public EntityManager()
         {
             _entities = new List<Entity>();
@@ -24,16 +18,8 @@ namespace DoomNG.Engine.Systems
             _components = new Dictionary<Entity, List<IComponent>>();
         }
 
-        /// <summary>
-        /// Executes this instance.
-        /// </summary>
         public void Execute() { }
 
-        /// <summary>
-        /// Creates the entity, attaches desired components
-        /// </summary>
-        /// <param name="components"> The components. </param>
-        /// <returns> The Entity. </returns>
         public Entity CreateEntity(params IComponent[] components)
         {
             var entity = new Entity(_nextId++);
@@ -45,12 +31,6 @@ namespace DoomNG.Engine.Systems
             return entity;
         }
 
-        /// <summary>
-        /// Sets the dictionaries for a entity to note the existence of, and the reference to its component
-        /// </summary>
-        /// <typeparam name="T"> The type of the component </typeparam>
-        /// <param name="entity"> The entity.</param>
-        /// <param name="component"> The component.</param>
         public void SetComponent<T>(Entity entity, T component) where T : IComponent
         {
             Type type = component.GetType();
@@ -64,22 +44,28 @@ namespace DoomNG.Engine.Systems
             _entitiesByType[type].Add(entity);
         }
 
-        /// <summary>
-        /// Gets the entities with desired Component
-        /// </summary>
-        /// <typeparam name="T"> Type of desired component </typeparam>
-        /// <returns> A List of Entities containing that component </returns>
         public List<Entity> GetEntitiesWith<T>() where T : IComponent
         {
             return _entitiesByType[typeof(T)];
         }
 
-        /// <summary>
-        /// Gets the component on an entity if it exists
-        /// </summary>
-        /// <typeparam name="T"> Type of desired component </typeparam>
-        /// <param name="entity"> The entity.</param>
-        /// <returns> The component desired, null otherwise </returns>
+        public List<Entity> GetEntitiesWith(params Type[] types)
+        {
+            List<Entity> retList = new List<Entity>();
+            foreach(Type t in types)
+            {
+                if(retList.Count == 0)
+                {
+                    retList = _entitiesByType[t];
+                }
+                else
+                {
+                    retList = retList.Intersect(_entitiesByType[t]).ToList();
+                }
+            }
+            return retList;
+        }
+
         public T GetComponent<T>(Entity entity) where T : IComponent
         {
             foreach(var component in _components[entity])
@@ -92,12 +78,6 @@ namespace DoomNG.Engine.Systems
             return default(T);
         }
 
-        /// <summary>
-        /// Checks if a entity is holding a component of said type
-        /// </summary>
-        /// <typeparam name="T"> Type of desired component </typeparam>
-        /// <param name="entity"> The entity.</param>
-        /// <returns> True if the entity contains a component of that type, false otherwise </returns>
         public bool HasComponent<T>(Entity entity) where T : IComponent
         {
             if (!_entitiesByType.ContainsKey(typeof(T))) return false;
